7669969a2d4c483c80a4e0d2decd59c5fe7d3861
Playwo <eliaswolf2001@t-online.de>
2020-11-12 18:11:02 +0100

    Add error handling for small cache size

--- InteractivityAddon/Extensions/SocketReactionExtensions.cs
+++ InteractivityAddon/Extensions/SocketReactionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord.WebSocket;
 
 namespace Interactivity.Extensions
@@ -11,6 +12,11 @@ namespace Interactivity.Extensions
             var message = reaction.Message.IsSpecified
                 ? reaction.Message.Value
                 : await channel.GetMessageAsync(reaction.MessageId).ConfigureAwait(false) as SocketUserMessage;
+
+            if (message == null)
+            {
+                throw new InvalidOperationException("Could not load the paginator message to an emote! Try increasing your Message Cache");
+            }
 
             await message.RemoveReactionAsync(reaction.Emote, reaction.UserId).ConfigureAwait(false);
         }
